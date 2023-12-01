6e11d3cbd3e14d1881d56326eb500701a09356d7
Vidrolll <vidrollyt@gmail.com>
2023-12-01 11:34:32 -0600

    Exported Jar File
    Signed-off-by: Vidrolll <vidrollyt@gmail.com>

---
+++ Tester/src/Main.java
@@ -0,0 +1,25 @@
+import com.gameengine.main.Game;
+import com.gameengine.main.util.RendererVariables;
+
+import java.awt.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main extends Game {
+    public Main(String gameName) {
+        super(gameName);
+        createWindow(800, 600);
+        start();
+        RendererVariables.ANTIALIASING = RenderingHints.VALUE_ANTIALIAS_ON;
+    }
+
+    public static void main(String[] args) {
+        new Main("Test Game");
+    }
+
+    @Override
+    public void drawForeground(Graphics2D g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(50, 50, 50, 50);
+    }
+}
